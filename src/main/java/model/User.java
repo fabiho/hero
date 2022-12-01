5b5f04d870e5b248529723b119a500caa650a9f6
Fabian Hofer <hofer-93@gmx.de>
2022-12-01 10:29:51 +0100

    Versuch Bugfix Login

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -5,14 +5,14 @@ import Login.SessionUtils;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-import javax.annotation.ManagedBean;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.inject.Named;
 import javax.servlet.http.HttpSession;
 import java.io.Serializable;
 
-@ManagedBean
+@Named
 @SessionScoped
 @Entity
 public class User implements Serializable {
