22746fc767635e774e153b77066dfd0aa36add7f
Fabian Hofer <hofer-93@gmx.de>
2022-12-01 17:50:40 +0100

    SelectOneMenu mit Robert versucht zu fixen. Listener ruft Methode noch nicht auf

---
+++ src/main/java/model/CountryConverter.java
@@ -0,0 +1,48 @@
+package model;
+
+import DAO.CountryDAO;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import java.util.List;
+
+@FacesConverter("countryConverter")
+public class CountryConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext context,
+                              UIComponent component,
+                              String value) {
+        //prepare your datasource ex. EJB .. etc
+        if (value.trim().equals("")) {
+            return null;
+        } else {
+            CountryDAO service = new CountryDAO();
+            List<Country> countries = service.findAll();
+
+            for(Country country : countries) {
+                if(country.getCountry_name().equals(value)) {
+                    return country;
+                }
+            }
+
+            return countries;
+        }
+
+    }
+
+    @Override
+    public String getAsString(FacesContext context,
+                              UIComponent component,
+                              Object value)   {
+        if (value == null || value.equals("")) {
+            return "";
+        } else {
+            return String.valueOf(((Country) value).getCountry_name());
+        }
+    }
+
+
+}
